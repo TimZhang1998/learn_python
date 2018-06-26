3b09008581ebf847c1710a1963f7f0ccfe6efa65
Zhang Chao <373930051@qq.com>
2018-06-26 12:08:39 +0800

    none

--- Python_spider/spider_for_jiepai/spider.py
+++ Python_spider/spider_for_jiepai/spider.py
@@ -2,26 +2,27 @@ from urllib.parse import urlencode
 from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import ErrorInResponseException
 from config import *
+from hashlib import md5
+from multiprocessing import Pool
 # import demjson
 import requests
 import json
 import re
 import pymongo
+import os
+
 
 client = pymongo.MongoClient(MONGO_URL)
 db = client[MONGO_DB]
 
+
 def get_page_index(offset, keyword):
     data = {
-        'offset': 0,
+        'offset': offset,
         'format': 'json',
-        'keyword': '街拍',
+        'keyword': keyword,
         'autoload': 'true',
         'count': '20',
         'cur_tab': 1,
@@ -66,12 +67,12 @@ def get_page_detail(url):
     '''
 
 def page_detail_parse(html, url):
-    data = []
+    images = []
     soup = BeautifulSoup(html, 'lxml')
     title = soup.select('title')[0].get_text()
     print(title)
     '''
-    images_pattern = re.compile(r'gallery: JSON.parse\("(.*?)"\)', re.S)
+    images_pattern = re.compile(r'gallery: JSON.parse("(.*?)")', re.S)    # 无法匹配到json字符串  
     result = re.search(images_pattern, html)
     if result:
         data = json.loads(result.group(1))
@@ -90,13 +91,32 @@ def page_detail_parse(html, url):
     if results:
         for result in results:
             image = result[1]
-            data.append(image)
-        if data:
+            images.append(image)
+            download_image(image)
+        if images:
             return {
                 'title': title,
                 'url': url,
-                'image': data
+                'images': images
                 }
+
+def download_image(url):
+    print('downloading ', url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            save_image(response.content)
+        return None
+    except RequestException:
+        print('request image error', url)
+        return None
+
+def save_image(content):
+    file_path = '{0}/{1}{2}'.format(os.getcwd(), md5(content).hexdigest(), 'jpg')
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+            f.close()
 
 def save_to_mongo(result):
     if db[MONGO_TABLE].insert(result):
@@ -104,17 +124,23 @@ def save_to_mongo(result):
         return None
     return False
 
-def main():
-    html = get_page_index(0, '街拍')
+
+def main(offset):
+    html = get_page_index(offset, KEYWORD)
     for url in parse_page_index(html):
         if url:
             html = get_page_detail(url)
             # print(html)
             if html:
                 result = page_detail_parse(html, url)
-                # print(result)
-                save_to_mongo(result)
+                if result:
+                    # print(result)
+                    save_to_mongo(result)
+
 
 if __name__ == '__main__':
-    main()
+    # main()
+    groups =[x*20 for x in range(GROUP_START, GROUP_END + 1)] 
+    pool = Pool()
+    pool.map(main, groups)
     
